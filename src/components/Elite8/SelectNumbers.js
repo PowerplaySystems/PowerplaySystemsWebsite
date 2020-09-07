9a002d6db7d973ce07c8244e166012be2524826f
DevLab09 <devlabsgear@gmail.com>
2020-09-07 19:01:50 +0300

    Final changes

--- src/components/Elite8/SelectNumbers.js
+++ src/components/Elite8/SelectNumbers.js
@@ -1,92 +1,194 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import "./index.css";
 
-export default function ComponentSelectNumbers(props) {
-  //state valiable 'selected'
-  //update this state varaible using 'setSelected' function call
-  //this way on a button call e.g.
-  // <button onClick={() => setSelected([4, 5 ,6 ])}}>
-  //default value is-> an empty array [], useState([]);
+class SelectNumbers extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const [selected, setSelected] = useState([]);
-  const [ballsArray, setBallsArray] = useState(initBalls());
+    //state valiable 'selected'
+    this.state = { selected: [] };
+  }
 
-  useEffect(() => {
-    // initBalls();
-  });
-
-  function Ball(props) {
+  render() {
     return (
-      <div className={"elite8-ball " + (props.isSelected ? "selected" : "")}>
-        {props.number}
-      </div>
+      <section>
+        <div className="PickNumbers-wrapper">
+          <div className="PickNumbers-left">
+            <div className="PickNumbers-header"> Pick Your Numbers </div>
+            <div className="PickNumbers-header-sub">
+              Select 8 numbers in total by clicking the the lottery balls below.
+            </div>
+            <div className="all-PickNumbers-box">{this.initBalls()}</div>
+          </div>
+          <div className="PickNumbers-right">
+            <div className="PickNumbers-right-heading">
+              You can use <span className="orange-text"> Powerplays </span> to
+              edit your selections during the live draw!
+            </div>
+            <div className="PickNumbers-right-img">
+              <img
+                src={require("../../assets/images/elite8-promo/group-34.png")}
+              />
+            </div>
+            <div className="PickNumbers-right-balls">
+              <div className="PickNumbers-right-balls-div">
+                {this.initSelectedBalls()}
+              </div>
+            </div>
+            <SubmitMyPicks />
+          </div>
+        </div>
+        <div className="elite8-about">
+          <div className="elite8-about-wrapper">
+            <div className="elite8-about-heading">Elite8</div>
+            <div className="elite8-about-subheading">
+              Our platform is designed for all PC and mobile devices, bringing
+              you access at any time, in any place, without boundaries.
+            </div>
+            <div className="elite8-about-table">
+              <div className="elite8-about-solo">
+                <div className="elite8-about-table-heading col1">
+                  Live Draw Starts At
+                </div>
+                <div className="elite8-about-table-text">
+                  9:00 pm EST on Feb 27th, 2020
+                </div>
+              </div>
+              <div className="elite8-about-solo">
+                <div className="elite8-about-table-heading col2">
+                  Deadline for choosing or modifying your picks
+                </div>
+                <div className="elite8-about-table-text">
+                  One hour before the draw (8:00PM ET)
+                </div>
+              </div>
+              <div className="elite8-about-solo">
+                <div className="elite8-about-table-heading col3">
+                  Contest closes at
+                </div>
+                <div className="elite8-about-table-text">
+                  8:00pm ET on April 26, 2020.
+                </div>
+              </div>
+            </div>
+            <div className="elite8-about-orange-bar"> </div>
+            <div className="elite8-about-list-wrapper">
+              <div className="elite8-about-list">
+                <div className="list-col1">
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    Thirty (30) prizes available to be won.
+                  </div>
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    One entry per person.
+                  </div>
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    Open to residents of Canada (excluding Quebec) &amp; United
+                    States who are over the age of majority.
+                  </div>
+                </div>
+                <div className="list-col2">
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    Skill-testing question must be correctly answered to win.
+                  </div>
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    Odds of winning depend on participants use of Powerplays.{" "}
+                  </div>
+                  <div className="elite8-about-list-item">
+                    <div className="elite8-about-list-oval" />
+                    No purchase necessary.
+                  </div>
+                </div>
+              </div>
+              <ContestRules />
+            </div>
+          </div>
+        </div>
+      </section>
     );
-    //  else return <div className="elite8-ball">{props.number}</div>;
-  }
-  function onBallClicked(number) {
-    console.log("Clicked", number, "selected:",  selected );
-    //check if selected array has the number
-    if (selected.includes(number)) {
-      //if yes, remove the number from array
-      removeFromSelectedNumbers(number);
-    } else {
-      //if no, add the number in array
-      addToSelectedNumbers(number);
-    }
-   
   }
 
-  function initBalls() {
+  onBallClicked(number) {
+    // check if selected array has the number
+    if (this.state.selected.includes(number)) {
+      // if yes, remove the number from array
+      this.removeFromSelectedNumbers(number);
+    } else {
+      if (this.state.selected.length > 7) return alert("Please shokhe na hun!");
+      // if no, add the number in array
+      this.addToSelectedNumbers(number);
+    }
+  }
+
+  initBalls() {
     var mArray = [];
     // function for loop of Balls
-
     for (let i = 1; i <= 46; i++) {
       mArray.push(
-        <div onClick={() => onBallClicked(i)}>
-          <Ball number={i} isSelected={selected.includes(i)} />
+        <div
+          className="elite8-ball-wrapper"
+          onClick={() => this.onBallClicked(i)}
+        >
+          <Ball number={i} isSelected={this.state.selected.includes(i)} />
         </div>
       );
     }
     return mArray;
   }
 
-  function addToSelectedNumbers(number) {
-    // tempArray.push(number);
-    //setSelected(selected.concat(number));
-    // var tArr = [...selected];
-    // tArr.push(number);
-    setSelected((selected) =>[...selected, selected.length,number]);
-    // console.log("add function", selected);
+  initSelectedBalls() {
+    var mArray = [];
+    // function for loop of Balls isSelected={selected.includes(i)}
+    for (let i = 0; i < this.state.selected.length; i++) {
+      mArray.push(
+        <div className="space-div">
+          <Ball number={this.state.selected[i]} isSelected={true} />
+        </div>
+      );
+    }
+
+    return mArray;
   }
 
-  function removeFromSelectedNumbers(number) {}
+  addToSelectedNumbers(number) {
+    // tempArray.push(number);
+    //setSelected(selected.concat(number));
+    var oldArray = [...this.state.selected];
+    oldArray.push(number);
+    this.setState({ selected: oldArray });
+  }
 
+  removeFromSelectedNumbers(number) {
+    var oldArray = [...this.state.selected];
+    const index = oldArray.indexOf(number);
+    if (index > -1) {
+      oldArray.splice(index, 1);
+    }
+
+    this.setState({ selected: oldArray });
+  }
+}
+function Ball(props) {
   return (
-    <section>
-      <div className="PickNumbers-wrapper">
-        <div className="PickNumbers-left">
-          <div className="PickNumbers-header"> Pick Your Numbers </div>
-          <div className="PickNumbers-header-sub">
-            Select 8 numbers in total by clicking the the lottery balls below.
-          </div>
-          <div className="all-PickNumbers-box">
-            {/* <CreateBalls /> */}
-            {ballsArray}
-          </div>
-        </div>
-        <div className="PickNumbers-right">
-          <div className="PickNumbers-right-heading">
-            You can use <span className="orange-text"> Powerplays </span> to
-            edit your selections during the live draw!
-          </div>
-          <div className="PickNumbers-right-img">
-            <img
-              src={require("../../assets/images/elite8-promo/group-34.png")}
-            />
-          </div>
-          <div className="PickNumbers-powerplays" />
-          <div className="PickNumbers-selected-numbers" />
-        </div>
-      </div>
-    </section>
+    <div className={"elite8-ball " + (props.isSelected ? "selected" : "")}>
+      {props.number}
+    </div>
+  );
+  //  else return <div className="elite8-ball">{props.number}</div>;
+}
+
+function SubmitMyPicks(props) {
+  return (
+    <button className="c-pick-your-numbers-btn submit">Submit My Picks</button>
   );
 }
+
+function ContestRules(props) {
+  return <button className="contest-rules-btn"> Contest Rules </button>;
+}
+
+export default SelectNumbers;
--- src/components/Elite8/index.js
+++ src/components/Elite8/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import SelectNumbers from "./SelectNumbers.js";
 import Header from "../common/Header.js";
 import Footer from "../common/Footer.js";
+import { useState, useEffect } from "react";
 
 function ComponentPickNumbersBtn(props) {
   return <button className="c-pick-your-numbers-btn">Pick Your Numbers</button>;
@@ -27,7 +28,6 @@ function StaticContent(props) {
               <span className="hero-bars" />
             </div>
             <div className="hero-text3">
-              {" "}
               $2000
               <span className="CAD"> CAD </span>
             </div>
@@ -39,18 +39,21 @@ function StaticContent(props) {
       <div className="fun-play-wrapper">
         <div className="fun-play-left">
           <div className="fun-play-heading"> A Fun New Way to Play! </div>
+          <div className="fun-play-phone mobile-only"> </div>
           <div className="fun-play-text1">
-            {" "}
             Have you ever played a pick-and-wait game like Super 7, 6/49, or
-            Powerball?{" "}
+            Powerball?
           </div>
           <div className="fun-play-text2a">
-            {" "}
             At PowerPlay Systems,
             <div className="fun-play-text2b">
               We think the current pick-and-wait format is kind of boring. So we
               invented a fun new way to play!
             </div>
+          </div>
+          <div className="fun-play-try mobile-only">
+            {" "}
+            Give it a try to see if you feel the same way.
           </div>
           <ComponentPickNumbersBtn />
         </div>
@@ -63,8 +66,7 @@ function StaticContent(props) {
       </div>
       <div className="edit-selections-wrapper">
         <div className="edit-selections-heading">
-          {" "}
-          Edit your selections during the live draw!{" "}
+          Edit your selections during the live draw!
         </div>
         <div className="edit-selections-subheading">
           To make it more exciting, you can make use of Powerplays during the
@@ -78,8 +80,7 @@ function StaticContent(props) {
             />
             <div className="edit-selections-box-heading"> Power Match </div>
             <div className="edit-selections-box-text">
-              {" "}
-              Use Power Match to match the in-play number.{" "}
+              Use Power Match to match the in-play number.
             </div>
           </div>
           <div className="edit-selections-box">
@@ -92,11 +93,10 @@ function StaticContent(props) {
               src={require("../../assets/images/elite8-promo/down-active.png")}
             />
             <div className="edit-selections-box-heading">
-              {" "}
-              Increase/ Decrease{" "}
+              Increase/ Decrease
             </div>
             <div className="edit-selections-box-text">
-              You can increase or decrease your pick during live draw.{" "}
+              You can increase or decrease your pick during live draw.
             </div>
           </div>
           <div className="edit-selections-box">
@@ -106,7 +106,7 @@ function StaticContent(props) {
             />
             <div className="edit-selections-box-heading"> Replace </div>
             <div className="edit-selections-box-text">
-              Used to replace one number with a random new Number.{" "}
+              Used to replace one number with a random new Number.
             </div>
           </div>
           <div className="edit-selections-box">
@@ -116,12 +116,11 @@ function StaticContent(props) {
             />
             <div className="edit-selections-box-heading"> Replace All </div>
             <div className="edit-selections-box-text">
-              Used to replace one number with a random new Number.{" "}
+              Used to replace one number with a random new Number.
             </div>
           </div>
         </div>
         <div className="edit-selections-bottom-text">
-          {" "}
           We will give you three free Powerplays to start!
         </div>
         <ComponentPickNumbersBtn />
@@ -130,9 +129,14 @@ function StaticContent(props) {
       <div className="match8-wrapper">
         <div className="match8-left">
           <div className="match8-heading">
-            {" "}
             Match 8 of 8 numbers &amp; Win
-            <span className="orange-text"> $2,000 </span> CAD{" "}
+            <span className="orange-text"> $2,000 </span> CAD
+          </div>
+          <div className="match8-right-mobile mobile-only">
+            <img
+              className="match8-right-mob-img"
+              src={require("../../assets/images/elite8-promo/lottery-orig-copy.png")}
+            />
           </div>
           <div className="match8-subheading"> Smaller prizes available </div>
           <div className="match8-box-container">
@@ -142,6 +146,7 @@ function StaticContent(props) {
             <div className="match8-brownbox bx2">$100</div>
           </div>
         </div>
+
         <div className="match8-right">
           <img
             src={require("../../assets/images/elite8-promo/lottery-orig-copy.png")}
